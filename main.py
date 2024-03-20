eda42b773991173b37224f1c1d22dc5b0fa8d6e9
qrivthv <pisofia@yandex.ru>
2024-03-20 21:15:37 +0300

    кусок фронтэнда

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from flask import Flask, render_template, redirect, abort, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
-from data import db_session, csv_api
-from data.users import User
-from data.companies import Company
-from data.lands import Land
-from data.user_to_company import UserCompany
-from form.user import RegisterForm, LoginForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 host = '127.0.0.1'
 port = '5000'
 path = f'http://{host}:{port}'
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=int(port), host=host)
