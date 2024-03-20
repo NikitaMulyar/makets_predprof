7b73f8951b05c4baaf01fcbdd2c46e7273980dd6
qrivthv <pisofia@yandex.ru>
2024-03-20 21:55:32 +0300

    кусок фронтэнда 2

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ path = f'http://{host}:{port}'
 def index():
     return render_template('index.html')
 
+@app.route('/cards')
+def cards():
+    return render_template('cards.html')
 
 if __name__ == '__main__':
     app.run(port=int(port), host=host)
