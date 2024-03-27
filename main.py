a85ea8963dfecd9709bab72ee48b588735ec1f2e
qrivthv <pisofia@yandex.ru>
2024-03-27 19:14:40 +0300

    макеты входа и регистрации (питон на 0)

--- main.py
+++ main.py
@@ -28,5 +28,13 @@ def cards():
 def user():
     return render_template('user.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(port=int(port), host=host)
